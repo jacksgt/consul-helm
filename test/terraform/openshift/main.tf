c6bcdaefac6d9eeb77c82744cf1b07d17d457eb8
Iryna Shustava <iryna@hashicorp.com>
2021-05-17 18:30:57 -0600

    wip: use template

--- test/terraform/openshift/main.tf
+++ test/terraform/openshift/main.tf
@@ -2,6 +2,10 @@ provider "azurerm" {
   features {}
 }
 
+provider "azuread" {
+  version = "1.4.0"
+}
+
 data "azurerm_client_config" "current" {}
 
 resource "random_id" "suffix" {
@@ -9,10 +13,22 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
+resource "random_string" "domain" {
+  count   = var.cluster_count
+  special = false
+  number  = false
+  upper   = false
+  length  = 6
+}
+
+locals {
+  openshift_resource_provider_application_id = "f1dd0a37-89c6-4e07-bcd1-ffd3d43d8875"
+}
+
 resource "azurerm_resource_group" "test" {
   count    = var.cluster_count
   name     = "consul-k8s-${random_id.suffix[count.index].dec}"
-  location = var.region
+  location = var.location
 }
 resource "azurerm_virtual_network" "test" {
   count               = var.cluster_count
@@ -41,36 +57,113 @@ resource "azurerm_subnet" "worker-subnet" {
   service_endpoints    = ["Microsoft.ContainerRegistry"]
 }
 
+//resource "azuread_application" "test" {
+//  count = var.cluster_count
+//  name  = "aro-consul-k8s-${random_id.suffix[count.index].dec}"
+//}
+//
+//resource "azuread_service_principal" "test" {
+//  count          = var.cluster_count
+//  application_id = azuread_application.test[count.index].application_id
+//}
+//
+//resource "random_password" "password" {
+//  count   = var.cluster_count
+//  length  = 16
+//  special = false
+//}
+//
+//resource "azuread_service_principal_password" "test" {
+//  count                = var.cluster_count
+//  service_principal_id = azuread_service_principal.test[count.index].id
+//  value                = random_password.password[count.index].result
+//  end_date             = "2099-01-01T01:02:03Z"
+//}
+
+data "azuread_service_principal" "openshift_rp" {
+  application_id = local.openshift_resource_provider_application_id
+}
+
+//data "azuread_service_principal" "client" {
+//  application_id = var.client_id
+//}
+
+//resource "azurerm_role_assignment" "vnet_assignment" {
+//  count                = var.cluster_count
+//  scope                = azurerm_virtual_network.test[count.index].id
+//  role_definition_name = "Network Contributor"
+//  principal_id         = data.azuread_service_principal.client.object_id
+//}
+
+resource "azurerm_role_assignment" "rp_assignment" {
+  count                = var.cluster_count
+  scope                = azurerm_virtual_network.test[count.index].id
+  role_definition_name = "Network Contributor"
+  principal_id         = data.azuread_service_principal.openshift_rp.object_id
+}
+
+resource "azurerm_template_deployment" "azure-arocluster" {
+  count               = var.cluster_count
+  name                = azurerm_resource_group.test[count.index].name
+  resource_group_name = azurerm_resource_group.test[count.index].name
+
+  template_body = file("./aro-template.json")
+
+  parameters = {
+    clusterName              = azurerm_resource_group.test[count.index].name
+    clusterResourceGroupName = join("-", [azurerm_resource_group.test[count.index].name, "MANAGED"])
+    location                 = var.location
+
+    tags = jsonencode(var.tags)
+
+    domain = random_string.domain[count.index].result
+
+    clientId     = var.client_id
+    clientSecret = var.client_secret
+
+    workerSubnetId = azurerm_subnet.worker-subnet[count.index].id
+    masterSubnetId = azurerm_subnet.master-subnet[count.index].id
+  }
+
+  deployment_mode = "Incremental"
+
+  depends_on = [
+    azurerm_role_assignment.rp_assignment,
+  ]
+}
+
 resource "null_resource" "aro" {
   count = var.cluster_count
 
   triggers = {
-    name          = azurerm_subnet.master-subnet[count.index].resource_group_name
-    master_subnet = azurerm_subnet.master-subnet[count.index].id
-    worker_subnet = azurerm_subnet.worker-subnet[count.index].id
+    aro_cluster = azurerm_template_deployment.azure-arocluster[count.index].name
+    //    name          = azurerm_subnet.master-subnet[count.index].resource_group_name
+    //    master_subnet = azurerm_subnet.master-subnet[count.index].id
+    //    worker_subnet = azurerm_subnet.worker-subnet[count.index].id
   }
 
-  # This is a horrible hack until terraform Azure provider officially supports this resource
-  # https://github.com/terraform-providers/terraform-provider-azurerm/issues/3614.
-  provisioner "local-exec" {
-    command = <<EOF
-    az aro create \
-      --resource-group ${azurerm_resource_group.test[count.index].name} \
-      --name ${self.triggers.name} \
-      --vnet ${azurerm_virtual_network.test[count.index].name} \
-      --vnet-resource-group ${azurerm_resource_group.test[count.index].name} \
-      --master-subnet master-subnet \
-      --worker-subnet worker-subnet
-    EOF
-  }
+  //  # This is a horrible hack until terraform Azure provider officially supports this resource
+  //  # https://github.com/terraform-providers/terraform-provider-azurerm/issues/3614.
+  //  provisioner "local-exec" {
+  //    command = <<EOF
+  //    az aro create \
+  //      --resource-group ${azurerm_resource_group.test[count.index].name} \
+  //      --name ${self.triggers.name} \
+  //      --vnet ${azurerm_virtual_network.test[count.index].name} \
+  //      --vnet-resource-group ${azurerm_resource_group.test[count.index].name} \
+  //      --master-subnet master-subnet \
+  //      --worker-subnet worker-subnet
+  //    EOF
+  //  }
 
   provisioner "local-exec" {
-    command     = "./oc-login.sh ${self.triggers.name} ${self.triggers.name}"
+    command     = "./oc-login.sh ${self.triggers.aro_cluster} ${self.triggers.aro_cluster}"
     interpreter = ["/bin/bash", "-c"]
+    on_failure  = continue
   }
 
-  provisioner "local-exec" {
-    when    = destroy
-    command = "az aro delete --resource-group ${self.triggers.name} --name ${self.triggers.name} --yes"
-  }
+  //  provisioner "local-exec" {
+  //    when    = destroy
+  //    command = "az aro delete --resource-group ${self.triggers.name} --name ${self.triggers.name} --yes"
+  //  }
 }
--- test/terraform/openshift/variables.tf
+++ test/terraform/openshift/variables.tf
@@ -1,9 +1,25 @@
-variable "region" {
-  default     = "West US 2"
+variable "location" {
+  default     = "westus2"
   description = "The Azure Region to create all resources in."
 }
 
 variable "cluster_count" {
   default     = 1
   description = "The number of OpenShift clusters to create."
+}
+
+variable "client_id" {
+  default     = ""
+  description = "The client ID of the service principal to be used by Kubernetes when creating Azure resources like load balancers."
+}
+
+variable "client_secret" {
+  default     = ""
+  description = "The client secret of the service principal to be used by Kubernetes when creating Azure resources like load balancers."
+}
+
+variable "tags" {
+  type        = map
+  default     = {}
+  description = "tags to attach to the created resources."
 }
