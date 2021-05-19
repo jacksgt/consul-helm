14e4bf498c7cba25d94b1b6ee8aba6396ce8e00b
Iryna Shustava <iryna@hashicorp.com>
2021-05-19 07:42:47 -0600

    Skip TestConnectInject_RestartConsulClients if tproxy is enabled

--- test/acceptance/tests/connect/connect_inject_test.go
+++ test/acceptance/tests/connect/connect_inject_test.go
@@ -199,10 +199,7 @@ func TestConnectInject_RestartConsulClients(t *testing.T) {
 	ctx := suite.Environment().DefaultContext(t)
 
 	helmValues := map[string]string{
-		"global.image":                        "hashicorp/consul:1.10.0-beta2",
-		"server.enterpriseLicense.secretName": "",
-		"server.enterpriseLicense.secretKey":  "",
-		"connectInject.enabled":               "true",
+		"connectInject.enabled": "true",
 	}
 
 	releaseName := helpers.RandomName()
